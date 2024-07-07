81dfafe9227a887ce3f912420e429bafd5b360ba
Ken Moini <ken@kenmoini.com>
2024-07-07 10:15:55 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ class App extends Tasks {
                             </Button>
                         </form>
                         <div className="tasks-list">
-                            {tasks.map((task) => (
+                            {tasks.tasks.map((task) => (
                                 <Paper key={task._id} className="task-item">
                                     <Checkbox
                                         checked={task.completed}
